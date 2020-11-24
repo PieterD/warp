97dee041695a1f55f7fdd09184165814d96b06c7
Pieter Droogendijk <pieter@binky.org.uk>
2020-11-24 10:24:13 +0100

    bl

--- pkg/ecs/ecs.go
+++ pkg/ecs/ecs.go
@@ -3,7 +3,6 @@ package ecs
 import (
 	"fmt"
 	"math/rand"
-	"sync"
 )
 
 type (
@@ -18,28 +17,26 @@ type (
 )
 
 type Repository struct {
-	lock       sync.RWMutex
-	entities   EntityIDSet
-	locations  map[EntityID]Location
+	entities  EntityIDSet
+	locations map[EntityID]Location
+	passables map[EntityID]Passable
+	walls     map[EntityID]Wall
+
 	rLocations map[Location]EntityIDSet
-	passables  map[EntityID]Passable
-	walls      map[EntityID]Wall
 }
 
 func NewRepository() *Repository {
 	return &Repository{
-		entities:   NewEntityIDSet(),
-		locations:  make(map[EntityID]Location),
+		entities:  NewEntityIDSet(),
+		locations: make(map[EntityID]Location),
+		passables: make(map[EntityID]Passable),
+		walls:     make(map[EntityID]Wall),
+
 		rLocations: make(map[Location]EntityIDSet),
-		passables:  make(map[EntityID]Passable),
-		walls:      make(map[EntityID]Wall),
 	}
 }
 
 func (repo *Repository) NewEntity() EntityID {
-	repo.lock.Lock()
-	defer repo.lock.Unlock()
-
 	for n := 0; n < 256; n++ {
 		randomId := EntityID(rand.Int63())
 		if _, ok := repo.entities[randomId]; ok {
@@ -52,17 +49,18 @@ func (repo *Repository) NewEntity() EntityID {
 }
 
 func (repo *Repository) EntityExists(id EntityID) bool {
-	repo.lock.RLock()
-	defer repo.lock.RUnlock()
-
 	_, ok := repo.entities[id]
 	return ok
 }
 
-func (repo *Repository) EntitiesByLocation(l Location) EntityIDSet {
-	repo.lock.Lock()
-	defer repo.lock.Unlock()
+func (repo *Repository) DelEntity(id EntityID) {
+	repo.DelLocation(id)
+	repo.DelPassable(id)
+	repo.DelWall(id)
+	delete(repo.entities, id)
+}
 
+func (repo *Repository) EntitiesByLocation(l Location) EntityIDSet {
 	oSet := repo.rLocations[l]
 	if oSet == nil {
 		return nil
@@ -75,17 +73,11 @@ func (repo *Repository) EntitiesByLocation(l Location) EntityIDSet {
 }
 
 func (repo *Repository) Location(id EntityID) (Location, bool) {
-	repo.lock.RLock()
-	defer repo.lock.RUnlock()
-
 	l, ok := repo.locations[id]
 	return l, ok
 }
 
 func (repo *Repository) PutLocation(id EntityID, l Location) {
-	repo.lock.Lock()
-	defer repo.lock.Unlock()
-
 	repo.entities[id] = struct{}{}
 	repo.locations[id] = l
 	s, ok := repo.rLocations[l]
@@ -97,9 +89,6 @@ func (repo *Repository) PutLocation(id EntityID, l Location) {
 }
 
 func (repo *Repository) DelLocation(id EntityID) {
-	repo.lock.Lock()
-	defer repo.lock.Unlock()
-
 	location, ok := repo.locations[id]
 	if !ok {
 		return
@@ -113,47 +102,29 @@ func (repo *Repository) DelLocation(id EntityID) {
 }
 
 func (repo *Repository) Passable(id EntityID) (Passable, bool) {
-	repo.lock.RLock()
-	defer repo.lock.RUnlock()
-
 	p, ok := repo.passables[id]
 	return p, ok
 }
 
 func (repo *Repository) PutPassable(id EntityID, p Passable) {
-	repo.lock.Lock()
-	defer repo.lock.Unlock()
-
 	repo.entities[id] = struct{}{}
 	repo.passables[id] = p
 }
 
 func (repo *Repository) DelPassable(id EntityID) {
-	repo.lock.Lock()
-	defer repo.lock.Unlock()
-
 	delete(repo.passables, id)
 }
 
 func (repo *Repository) Wall(id EntityID) (Wall, bool) {
-	repo.lock.RLock()
-	defer repo.lock.RUnlock()
-
 	w, ok := repo.walls[id]
 	return w, ok
 }
 
 func (repo *Repository) PutWall(id EntityID, w Wall) {
-	repo.lock.Lock()
-	defer repo.lock.Unlock()
-
 	repo.entities[id] = struct{}{}
 	repo.walls[id] = w
 }
 
 func (repo *Repository) DelWall(id EntityID) {
-	repo.lock.Lock()
-	defer repo.lock.Unlock()
-
 	delete(repo.walls, id)
 }
